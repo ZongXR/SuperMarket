c3774088cbb5de62bcda2b67021466b0bdaa00c2
zong.xiangrui <zxr@tju.edu.cn>
2022-03-24 22:43:14 +0800

    完善elasticsearch启动流程
    set max vm memory, put es to auto start up

--- docker/elasticsearch/start_es.sh
+++ docker/elasticsearch/start_es.sh
@@ -1,10 +1,14 @@
 # create elasticsearch subnet
 docker network create --driver bridge --subnet 192.168.0.0/16 --gateway 192.168.0.1 net-es
 
+# set the vm memory
+sysctl -w vm.max_map_count=262144
+
 # start es nodes
-docker run -d --name es-1 --net net-es -p 9201:9200 -p 9301:9300 -e "discovery.zen.ping.unicast.hosts=es-2,es-3" zongxr/elasticsearch:6.8.6
-docker run -d --name es-2 --net net-es -p 9202:9200 -p 9302:9300 -e "discovery.zen.ping.unicast.hosts=es-1,es-3" zongxr/elasticsearch:6.8.6
-docker run -d --name es-3 --net net-es -p 9200:9200 -p 9300:9300 -e "discovery.zen.ping.unicast.hosts=es-1,es-2" zongxr/elasticsearch:6.8.6
+docker run -d --name es-1 --restart always --net net-es -p 9201:9200 -p 9301:9300 -e "discovery.zen.ping.unicast.hosts=es-2,es-3" zongxr/elasticsearch:6.8.6
+docker run -d --name es-2 --restart always --net net-es -p 9202:9200 -p 9302:9300 -e "discovery.zen.ping.unicast.hosts=es-1,es-3" zongxr/elasticsearch:6.8.6
+docker run -d --name es-3 --restart always --net net-es -p 9200:9200 -p 9300:9300 -e "discovery.zen.ping.unicast.hosts=es-1,es-2" zongxr/elasticsearch:6.8.6
+sleep 20s
 
 # check the cluster
 docker exec es-1 curl -XGET 'http://es-3:9200/_cluster/health?pretty'
