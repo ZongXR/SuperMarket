6e0e448f85c69c78621a29f01f9af1bec57e2535
zong.xiangrui <zxr@tju.edu.cn>
2022-03-24 23:35:44 +0800

    完善mysql, es启动流程
    COPY docker-entrypoint之后需要做chmod，elasticsearch.yml的node.name属性设置为容器id，永久修改vm.max_map_count属性(需要root权限)

--- docker/elasticsearch/start_es.sh
+++ docker/elasticsearch/start_es.sh
@@ -2,13 +2,14 @@
 docker network create --driver bridge --subnet 192.168.0.0/16 --gateway 192.168.0.1 net-es
 
 # set the vm memory
+echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 sysctl -w vm.max_map_count=262144
 
 # start es nodes
 docker run -d --name es-1 --restart always --net net-es -p 9201:9200 -p 9301:9300 -e "discovery.zen.ping.unicast.hosts=es-2,es-3" zongxr/elasticsearch:6.8.6
 docker run -d --name es-2 --restart always --net net-es -p 9202:9200 -p 9302:9300 -e "discovery.zen.ping.unicast.hosts=es-1,es-3" zongxr/elasticsearch:6.8.6
 docker run -d --name es-3 --restart always --net net-es -p 9200:9200 -p 9300:9300 -e "discovery.zen.ping.unicast.hosts=es-1,es-2" zongxr/elasticsearch:6.8.6
-sleep 20s
+sleep 30s
 
 # check the cluster
 docker exec es-1 curl -XGET 'http://es-3:9200/_cluster/health?pretty'
