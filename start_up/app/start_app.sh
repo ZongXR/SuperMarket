ef9177e7f1f4496b6c1e689414344684750e40d5
zong.xiangrui <zxr@tju.edu.cn>
2022-03-26 11:53:46 +0800

    购物车微服务迁移至docker
    build supermarket-cart image, rename docker to start_up

--- start_up/app/start_app.sh
+++ start_up/app/start_app.sh
@@ -6,3 +6,7 @@ docker run -d --name supermarket-eureka-1 --restart always --net net-app -p 1000
 docker run -d --name supermarket-product-1 --restart always --net net-app -p 10002:10002 zongxr/supermarket-product:1.0-SNAPSHOT
 docker network connect net-mysql supermarket-product-1
 docker network connect net-redis supermarket-product-1
+
+# 启动购物车微服务
+docker run -d --name supermarket-cart-1 --restart always --net net-app -p 10005:10005 zongxr/supermarket-cart:1.0-SNAPSHOT
+docker network connect net-mysql supermarket-cart-1
